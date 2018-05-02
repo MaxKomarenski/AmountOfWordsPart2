7f31fee59fa99058b28969d5ab293e14e7dd3f5a
MaxKomarenski <komarenski@ucu.edu.ua>
2018-05-02 13:53:01 +0300

    sorting and writing into file

--- main.cpp
+++ main.cpp
@@ -5,6 +5,20 @@
 #include "Configuration.h"
 #include "FileWriter.h"
 
+inline std::chrono::high_resolution_clock::time_point get_current_time_fenced()
+{
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    auto res_time = std::chrono::high_resolution_clock::now();
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    return res_time;
+}
+
+template<class D>
+inline long long to_us(const D& d)
+{
+    return std::chrono::duration_cast<std::chrono::microseconds>(d).count();
+}
+
 
 void configure(Configuration& conf);
 
@@ -18,6 +32,9 @@ int main() {
     }
 
     std::cout<<conf;
+
+    auto stage1_start_time = get_current_time_fenced(); //time point
+
 
     Queue queue;
     FileReader  fileReader(1000);
@@ -39,13 +56,16 @@ int main() {
     }
 
 
+    auto finish_time = get_current_time_fenced();
+    auto total_time = finish_time - stage1_start_time;
+
     FileWriter fileWriter;
     auto target = mapsQueue.pop();
 
     fileWriter.sort_by_letters_and_write_into_file(target, conf.getFileWriteSortedByLetters());
     fileWriter.sort_by_amount_and_write_into_file(target, conf.getFileWriteSortedByAmount());
 
-
+    std::cout << "\nTime: " << to_us(total_time) << std::endl;
 
     return 0;
 }
